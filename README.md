de26316d3dd5e9caf15a52bbf417b52b482d7ad9
Tatiane Patriota <tatianepatriota1@gmail.com>
2022-08-24 17:35:18 -0300

    projeto finalizado

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Projeto em HTML e CSS, trabalhando responsividade. Começando por Mobile frist, o tornando responsivo em tablets e desktop.
+Projeto em HTML e CSS, trabalhando responsividade. Começando por Mobile frist, o tornando responsivo em tablets e desktop, trabalhando com fetch, try e catch para solução de erros com o cep.
---
+++ script.js
@@ -0,0 +1,30 @@
+async function buscaEndereco(cep) {
+  var mensagemErro = document.getElementById("erro");
+  mensagemErro.innerHTML = "";
+
+  try {
+    var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    var consultaCepConvertida = await consultaCep.json();
+    if (consultaCepConvertida.erro) {
+      throw Error("Cep não existente!");
+    }
+    var cidade = document.getElementById("cidade");
+    var logradouro = document.getElementById("endereco");
+    var bairro = document.getElementById("bairro");
+    var estado = document.getElementById("estado");
+
+    cidade.value = consultaCepConvertida.localidade;
+    logradouro.value = consultaCepConvertida.logradouro;
+    bairro.value = consultaCepConvertida.bairro;
+    estado.value = consultaCepConvertida.uf;
+
+    console.log(consultaCepConvertida);
+    return consultaCepConvertida;
+  } catch (erro) {
+    mensagemErro.innerHTML = `<p>Cep inválido. Tente novamente!</p>`;
+    console.log(erro);
+  }
+}
+
+var cep = document.getElementById("cep");
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
